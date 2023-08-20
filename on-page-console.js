2979663d9b6ab968f044caaf1a4e7f05ac741183
Aswani Vonteddu <ice_cold_aswin@hotmail.com>
2023-08-20 15:44:24 -0700

    [on-page-console] Include font-awesome css

--- on-page-console.js
+++ on-page-console.js
@@ -48,10 +48,16 @@ var clearconsole = () => document.getElementById('console-log').innerHTML = "";
 var createConsole = () => {
     var head  = document.getElementsByTagName('head')[0];
     var link  = document.createElement('link');
-    
     link.rel  = 'stylesheet';
     link.type = 'text/css';
     link.href = 'https://icecoldaswin.github.io/a1-js-utils/on-page-console.css';
+    link.media = 'all';
+    head.appendChild(link);
+
+    link  = document.createElement('link');
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
     link.media = 'all';
     head.appendChild(link);
 
